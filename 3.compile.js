d7ed92306535b68413a910759e8f5fe7333568b1
dongo92 <dongo92@gmail.com>
2024-03-08 19:45:20 +0900

    Create 3.compile.js

---
+++ 3.compile.js
@@ -0,0 +1,15 @@
+
+
+const Web3 = require("web3");
+const web3 = new Web3(web3Provider);
+
+async function run() {
+
+    // auto-compile 이용
+ 
+
+}
+
+
+console.log("\n############### NEW EXECUTION #################\n");
+run();
